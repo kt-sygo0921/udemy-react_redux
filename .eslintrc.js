b300538054d0b22560f2e937ffd8991c358d3d0e
加藤翔吾 <s.kato2104@gmail.com>
2018-01-09 00:54:02 +0900

    section3-26

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser":true,
+        "es6": true,
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "2"
+        ]
+    },
+    'parserOptions': {
+        "sourceType": "module"
+    }
+};
